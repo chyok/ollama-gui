99f6091f5832bfb0f441a82dc6ed05d7be6d74f5
chyok <chyok@hotmail.com>
2024-06-30 16:17:32 +0800

    Optimize code logic

--- ollama_gui.py
+++ ollama_gui.py
@@ -13,19 +13,15 @@ from contextlib import contextmanager
 
 
 @contextmanager
-def widget_state_manager(self: "AIChatInterface",
-                         enable_sending: bool,
-                         enable_typing: bool):
+def widget_state_manager(self: "AIChatInterface"):
     try:
-        if enable_typing:
-            self.chat.config(state=tk.NORMAL)
-        if not enable_sending:
-            self.send_button.state(['disabled'])
+        self.chat_box.config(state=tk.NORMAL)
+        self.send_button.state(['disabled'])
         yield
     except Exception as _:
-        self.chat.insert(tk.END, f"\nAI error!\n\n", ("error",))
+        self.chat_box.insert(tk.END, f"\nAI error!\n\n", ("error",))
     finally:
-        self.chat.config(state=tk.DISABLED)
+        self.chat_box.config(state=tk.DISABLED)
         self.send_button.state(['!disabled'])
 
 
@@ -37,17 +33,9 @@ class Sender(Enum):
 class AIChatInterface:
     def __init__(self, root, background):
         self.root = root
-        self.default_font = font.nametofont('TkTextFont').actual()["family"]
-        root.title("Ollama GUI")
-        root.geometry("800x600")
-        root.configure(bg=background)
-
         self.api_url = 'http://localhost:11434'
         self.chat_history = []
-
-        root.grid_columnconfigure(0, weight=1)
-        root.grid_rowconfigure(1, weight=1)
-        root.grid_rowconfigure(2, weight=0)
+        self.default_font = font.nametofont('TkTextFont').actual()["family"]
 
         # header
         header_frame = ttk.Frame(root, style="TFrame")
@@ -79,13 +67,13 @@ class AIChatInterface:
         chat_frame.grid_columnconfigure(0, weight=1)
         chat_frame.grid_rowconfigure(0, weight=1)
 
-        self.chat = tk.Text(chat_frame, wrap=tk.WORD, state=tk.DISABLED, font=(self.default_font, 12))
-        self.chat.grid(row=0, column=0, sticky="nsew")
+        self.chat_box = tk.Text(chat_frame, wrap=tk.WORD, state=tk.DISABLED, font=(self.default_font, 12))
+        self.chat_box.grid(row=0, column=0, sticky="nsew")
 
-        scrollbar = ttk.Scrollbar(chat_frame, orient="vertical", command=self.chat.yview)
+        scrollbar = ttk.Scrollbar(chat_frame, orient="vertical", command=self.chat_box.yview)
         scrollbar.grid(row=0, column=1, sticky="ns")
 
-        self.chat.configure(yscrollcommand=scrollbar.set)
+        self.chat_box.configure(yscrollcommand=scrollbar.set)
 
         # input area
         input_frame = ttk.Frame(root)
@@ -96,7 +84,8 @@ class AIChatInterface:
         self.user_input.grid(row=0, column=0, sticky="ew", padx=(0, 10))
         self.user_input.bind("<Key>", self.handle_key_press)
 
-        self.send_button = ttk.Button(input_frame, text="  Send \n<Enter>", command=self.send_message, style="TButton")
+        self.send_button = ttk.Button(input_frame, text="  Send \n<Enter>", command=self.on_send_button,
+                                      style="TButton")
         self.send_button.grid(row=0, column=1)
         self.send_button.state(['disabled'])
 
@@ -109,7 +98,7 @@ class AIChatInterface:
             else:
                 if self.send_button.state() == ('disabled',):
                     return "break"
-                self.send_message(event)
+                self.on_send_button(event)
             return "break"
 
     def refresh_models(self):
@@ -145,7 +134,7 @@ class AIChatInterface:
         self.send_button.state(['disabled'])
         self.error_label.config(text="error")
 
-    def send_message(self, _=None):
+    def on_send_button(self, _=None):
         message = self.user_input.get("1.0", "end-1c").strip()
         if message:
             self.add_message_to_chat(Sender.User, message)
@@ -156,21 +145,21 @@ class AIChatInterface:
                    daemon=True).start()
 
     def add_message_to_chat(self, sender: Enum, message: Union[str, Generator]):
-        with widget_state_manager(self, enable_typing=True, enable_sending=False):
+        with widget_state_manager(self):
             _sender_name = sender.name if sender == Sender.User else f"AI ({self.model_select.get()})"
-            self.chat.insert(tk.END, f"{_sender_name}: \n", ("bold", sender.name))
+            self.chat_box.insert(tk.END, f"{_sender_name}: \n", ("bold", sender.name))
             if isinstance(message, str):
-                self.chat.insert(tk.END, f"{message}")
+                self.chat_box.insert(tk.END, f"{message}")
                 self.chat_history.append({"role": "user", "content": message})
             else:
                 ai_message = ""
                 for i in message:
-                    self.chat.insert(tk.END, f"{i}")
+                    self.chat_box.insert(tk.END, f"{i}")
                     ai_message += i
                 self.chat_history.append({"role": "assistant", "content": ai_message})
 
-            self.chat.insert(tk.END, "\n\n")
-            self.chat.see(tk.END)
+            self.chat_box.insert(tk.END, "\n\n")
+            self.chat_box.see(tk.END)
 
     def generate_ai_response(self):
         request = urllib.request.Request(
@@ -183,43 +172,43 @@ class AIChatInterface:
             headers={'Content-Type': 'application/json'},
             method='POST'
         )
-
         with urllib.request.urlopen(request) as resp:
-            buffer = b''
-            while True:
-                data = resp.read(1024)
-                if not data:
-                    break
-                buffer += data
-                while b'\n' in buffer:
-                    line, buffer = buffer.split(b'\n', 1)
-                    line = line.decode('utf-8')
-                    if line.strip():
-                        data = json.loads(line)
-                        if 'message' in data:
-                            time.sleep(0.01)  # add a small delay to improve readability
-                            yield data['message']['content']
+            for line in resp:
+                data = json.loads(line.decode('utf-8'))
+                if 'message' in data:
+                    time.sleep(0.01)
+                    yield data['message']['content']
 
     def clear_chat(self):
-        with widget_state_manager(self, enable_typing=True, enable_sending=False):
-            self.chat.delete(1.0, tk.END)
+        with widget_state_manager(self):
+            self.chat_box.delete(1.0, tk.END)
         self.chat_history.clear()
 
 
 def run():
     root = tk.Tk()
-    main_color = "#e1e0db"
-    app = AIChatInterface(root, background=main_color)
-    style = ttk.Style()
-    style.configure("TFrame", background=main_color)
-    style.configure("TButton", background=main_color)
-    style.configure("TLabel", background=main_color)
-    style.configure("TCombobox", background=main_color)
+    bg_color = "#e1e0db"
 
-    app.chat.tag_configure("bold", font=(app.default_font, 12, "bold"))
-    app.chat.tag_configure(Sender.User.name, foreground="#007bff")
-    app.chat.tag_configure(Sender.AI.name, foreground="#ff007b")
-    app.chat.tag_configure("error", foreground="red")
+    root.title("Ollama GUI")
+    root.geometry("800x600")
+    root.configure(bg=bg_color)
+    root.grid_columnconfigure(0, weight=1)
+    root.grid_rowconfigure(1, weight=1)
+    root.grid_rowconfigure(2, weight=0)
+
+    app = AIChatInterface(root, background=bg_color)
+
+    style = ttk.Style()
+
+    style.configure("TFrame", background=bg_color)
+    style.configure("TButton", background=bg_color)
+    style.configure("TLabel", background=bg_color)
+    style.configure("TCombobox", background=bg_color)
+
+    app.chat_box.tag_configure("bold", font=(app.default_font, 12, "bold"))
+    app.chat_box.tag_configure(Sender.User.name, foreground="#007bff")
+    app.chat_box.tag_configure(Sender.AI.name, foreground="#ff007b")
+    app.chat_box.tag_configure("error", foreground="red")
 
     root.mainloop()
 
